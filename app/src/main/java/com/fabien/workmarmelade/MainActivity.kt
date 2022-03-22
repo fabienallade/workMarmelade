a152f4c21a2ca674b8f17c9c5f7c3d4857ea1a13
Allade fabien <alladefabien@gmail.com>
2022-03-22 02:06:27 +0100

    work on project logic and finish logic

--- app/src/main/java/com/fabien/workmarmelade/MainActivity.kt
+++ app/src/main/java/com/fabien/workmarmelade/MainActivity.kt
@@ -27,6 +27,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import appolloClient
 import com.apollographql.apollo3.exception.ApolloException
 import com.fabien.workmarmelade.model.RandomQuote
@@ -41,12 +45,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WorkMarmeladeTheme {
+                val navController = rememberNavController()
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    NavigationComponent(navController = navController)
                 }
             }
         }
@@ -54,69 +59,141 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
+fun NavigationComponent(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "greeting") {
+        composable("greeting") {
+            Greeting(navController = navController)
+        }
+        composable("afterEnd") {
+            AfterEnd(navController = navController)
+        }
+    }
+}
+
+@Composable
+fun AfterEnd(navController: NavHostController) {
+    Box(modifier = Modifier.fillMaxSize().background(
+        brush = Brush.radialGradient(
+            colors = listOf(
+                BackgroundNew1,
+                BackgroundNew2
+            )
+        )
+    )) {
+        Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "Vous a fini le travail",
+                fontSize = 38.sp,
+                fontFamily = fontSansFamily,
+                fontWeight = FontWeight.Bold, color = Color.White)
+            Text(text = "A bientot",fontSize = 38.sp,
+                fontFamily = fontSansFamily,
+                fontWeight = FontWeight.Bold, color = Color.White)
+        }
+    }
+}
+
+@Composable
+fun Greeting(navController: NavHostController) {
 //    Configuration des données a afficher par default
 
     val percentagePerEvolve = 10
-    var percentage = remember{mutableStateOf<Int>(0)}
-    val phraseEvolution = listOf<String>("Citation suivante","finir")
-    val imagePhase = listOf<Int>(R.drawable.smiley_sick,R.drawable.smiley_meh,R.drawable.smiley_awe)
-    var imageChange : MutableState<Int> =remember{mutableStateOf<Int>(0)}
-    var buttonChange : MutableState<Int> =remember{mutableStateOf<Int>(0)}
-    var dialogOpen: MutableState<Boolean> = remember{mutableStateOf<Boolean>(true)}
+    var percentage = remember { mutableStateOf<Int>(0) }
+    val phraseEvolution = listOf<String>("CITATION SUIVANTE", "FINIT")
+    val imagePhase =
+        listOf<Int>(R.drawable.smiley_sick, R.drawable.smiley_meh, R.drawable.smiley_awe)
+    var imageChange: MutableState<Int> = remember { mutableStateOf<Int>(0) }
+    var buttonChange: MutableState<Int> = remember { mutableStateOf<Int>(0) }
+    var dialogOpen: MutableState<Boolean> = remember { mutableStateOf<Boolean>(false) }
     val coroutineScope = rememberCoroutineScope()
-    var data  = remember{
-        mutableStateOf<RandomQuoteQuery.RandomQuote>(RandomQuoteQuery.RandomQuote("1","fabien","fabien"))
+    var data = remember {
+        mutableStateOf<RandomQuoteQuery.RandomQuote>(
+            RandomQuoteQuery.RandomQuote(
+                "1",
+                "fabien",
+                "fabien"
+            )
+        )
     }
-    LaunchedEffect(key1 = data,){
+    LaunchedEffect(key1 = data) {
         data.value = quoteWork()
     }
 
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .background(
-            brush = Brush.verticalGradient(
-                colors = listOf(
-                    BackgroundNew1,
-                    BackgroundNew2
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        BackgroundNew1,
+                        BackgroundNew2
+                    )
                 )
             )
-        )){
-        Box(modifier = Modifier
-            .fillMaxWidth()
-            .height(height = 282.dp)
-            ){
-            Row(modifier = Modifier.padding(horizontal = 10.dp), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
-                Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-                    Text(text = "${data.value?.author}", fontSize = 38.sp,
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(height = 282.dp)
+        ) {
+            Row(
+                modifier = Modifier.padding(horizontal = 10.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "${data.value?.author}", fontSize = 38.sp,
                         fontFamily = fontSansFamily,
-                        fontWeight = FontWeight.Bold, color = Color.White)
-                    Text(text = "${data.value?.quote}",
-                        textAlign=TextAlign.Center,
-                        fontFamily = fontSansFamily,fontSize = 20.sp, fontWeight = FontWeight.Bold, color = Color.White)
+                        fontWeight = FontWeight.Bold, color = Color.White
+                    )
+                    Text(
+                        text = "${data.value?.quote}",
+                        textAlign = TextAlign.Center,
+                        fontFamily = fontSansFamily,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
                 }
             }
         }
-        Box(modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-            .background(
-                color = Color.White,
-                shape = RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp)
-            )){
-            Box(modifier = Modifier.fillMaxSize(),) {
-                Image(painterResource(id = imagePhase[imageChange.value]),
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .background(
+                    color = Color.White,
+                    shape = RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp)
+                )
+        ) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                Image(
+                    painterResource(id = imagePhase[imageChange.value]),
                     contentDescription = null,
                     modifier = Modifier
                         .align(Alignment.TopCenter)
                         .offset(y = -70.dp)
                         .height(131.dp)
-                        .width(156.dp))
+                        .width(156.dp)
+                )
             }
             Column(modifier = Modifier) {
                 Spacer(modifier = Modifier.height(70.dp))
-                Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.Bottom) {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.Bottom
+                    ) {
 //                        Text(text = "${percentage.value}%", fontSize = 38.sp,
 //                        fontWeight = FontWeight.Bold, color = TextColor,
 //                        )
@@ -124,8 +201,14 @@ fun Greeting(name: String) {
                         Text(
                             buildAnnotatedString {
                                 withStyle(style = ParagraphStyle(lineHeight = 30.sp)) {
-                                    withStyle(style = SpanStyle(color = TextColor
-                                        ,fontFamily = fontSansFamily,fontWeight = FontWeight.Bold,fontSize = 38.sp,)) {
+                                    withStyle(
+                                        style = SpanStyle(
+                                            color = TextColor,
+                                            fontFamily = fontSansFamily,
+                                            fontWeight = FontWeight.Bold,
+                                            fontSize = 38.sp,
+                                        )
+                                    ) {
                                         append("${percentage.value}% ")
                                     }
                                     withStyle(
@@ -143,13 +226,23 @@ fun Greeting(name: String) {
                     }
                 }
                 Spacer(modifier = Modifier.height(31.dp))
-                Column(modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 13.dp)) {
-                    Text(text = "Votre progression",fontFamily = fontSansFamily,color= TextColor, fontWeight = FontWeight.Bold)
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 13.dp)
+                ) {
+                    Text(
+                        text = "Votre progression",
+                        fontFamily = fontSansFamily,
+                        color = TextColor,
+                        fontWeight = FontWeight.Bold
+                    )
                     Spacer(modifier = Modifier.height(25.dp))
-                    Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth(),
-                        verticalAlignment = Alignment.CenterVertically) {
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceEvenly,
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
                         Column(modifier = Modifier.fillMaxWidth(0.94f)) {
                             CustomProgressBar(
                                 Modifier
@@ -158,47 +251,70 @@ fun Greeting(name: String) {
                                 350.dp,
                                 ProgressBackground,
                                 BackgroundNew1,
-                                percentage.value,)
+                                percentage.value,
+                            )
                         }
                         Column(modifier = Modifier.fillMaxWidth()) {
-                            Image(painterResource(id = R.drawable.picto_etoile),
+                            Image(
+                                painterResource(id = R.drawable.picto_etoile),
                                 contentDescription = null,
                                 modifier = Modifier
                                     .fillMaxWidth()
-                                    .height(17.dp))
+                                    .height(17.dp)
+                            )
                         }
                     }
                     Spacer(modifier = Modifier.height(5.dp))
-                    Column(horizontalAlignment = Alignment.End, modifier = Modifier.fillMaxWidth()) {
-                        Text(text = "10 Citations", fontSize = 13.sp,fontFamily = fontSansFamily,)
+                    Column(
+                        horizontalAlignment = Alignment.End,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(text = "10 Citations", fontSize = 13.sp, fontFamily = fontSansFamily)
                     }
                     Spacer(modifier = Modifier.height(20.dp))
-                    Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                        Button(onClick = {
-                            var percent = percentage.value+percentagePerEvolve
-                             coroutineScope.launch {
-                                 data.value = quoteWork()
-//                                 dialogOpen.value = true
-                                 percentage.value =percent
-                                 if(percent <= 39 && percent >= 0){
-                                     imageChange.value = 0
-                                 }else if(percent <= 79 && percent >= 40){
-                                     imageChange.value = 1
-                                 }else if(percent <= 100 && percent >= 80){
-                                     imageChange.value =2
-                                 }
-                             }
+                    Column(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Button(
+                            onClick = {
+                                var percent = percentage.value + percentagePerEvolve
+                                coroutineScope.launch {
+                                    if (percent > 100) {
+                                        navController.navigate("afterEnd")
+                                    } else {
+                                        data.value = quoteWork()
+                                        percentage.value = percent
+                                        if (percent <= 39 && percent >= 0) {
+                                            imageChange.value = 0
+                                        } else if (percent <= 79 && percent >= 40) {
+                                            imageChange.value = 1
+                                        } else if (percent <= 100 && percent >= 80) {
+                                            imageChange.value = 2
+                                        }
+                                        if (percent == 50) {
+                                            dialogOpen.value = true
+                                        }
+                                        if (percent == 100) {
+                                            buttonChange.value = 1
+                                        }
+                                    }
 
-                        },
+                                }
+
+                            },
                             colors = ButtonDefaults.buttonColors(backgroundColor = ButtonColor),
                             modifier = Modifier
                                 .clip(
                                     RoundedCornerShape(50)
                                 )
-                                .background(color = ButtonColor)) {
-                            Text(text = "${phraseEvolution[buttonChange.value]}",
+                                .background(color = ButtonColor)
+                        ) {
+                            Text(
+                                text = "${phraseEvolution[buttonChange.value]}",
                                 fontFamily = fontSansFamily,
-                            fontSize = 12.sp, color = Color.White)
+                                fontSize = 12.sp, color = Color.White
+                            )
                         }
                     }
                 }
@@ -206,9 +322,15 @@ fun Greeting(name: String) {
             }
 
         }
-//        Dialog(onDismissRequest = {dialogOpen.value = false}){
-//            CustomModal(modifier = Modifier, openDialogCustom = dialogOpen)
-//        }
+
+        if (dialogOpen.value) {
+            Dialog(onDismissRequest = { dialogOpen.value = false }) {
+                CustomModal(
+                    modifier = Modifier,
+                    openDialogCustom = dialogOpen, navController = navController
+                )
+            }
+        }
     }
 }
 
@@ -216,17 +338,27 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     WorkMarmeladeTheme {
-        Greeting("Android")
+        val navController = rememberNavController()
+        Greeting(navController)
     }
 }
 
- suspend fun quoteWork(): RandomQuoteQuery.RandomQuote {
-    val response =  try {
-       appolloClient.query(RandomQuoteQuery()).execute()
-    }catch (e:ApolloException){
+@Preview(showBackground = true)
+@Composable
+fun EndPreview() {
+    WorkMarmeladeTheme {
+        val navController = rememberNavController()
+        AfterEnd(navController)
+    }
+}
+
+suspend fun quoteWork(): RandomQuoteQuery.RandomQuote {
+    val response = try {
+        appolloClient.query(RandomQuoteQuery()).execute()
+    } catch (e: ApolloException) {
         Log.d("LaunchList", "Error ${e}")
         null
     }
-    Log.d("fabien","fabien ${response?.data}")
-     return (response?.data?.randomQuote ?: RandomQuoteQuery.RandomQuote("","",""))
+    Log.d("fabien", "fabien ${response?.data}")
+    return (response?.data?.randomQuote ?: RandomQuoteQuery.RandomQuote("", "", ""))
 }
--- app/src/main/java/com/fabien/workmarmelade/ui/theme/util/CustomModal.kt
+++ app/src/main/java/com/fabien/workmarmelade/ui/theme/util/CustomModal.kt
@@ -12,15 +12,21 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.*
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.fabien.workmarmelade.ui.theme.BackgroundNew1
 import com.fabien.workmarmelade.ui.theme.ProgressBackground
 import com.fabien.workmarmelade.ui.theme.TextColor
 import com.fabien.workmarmelade.ui.theme.fontSansFamily
 
 @Composable
-fun CustomModal(modifier: Modifier = Modifier, openDialogCustom: MutableState<Boolean>) {
+fun CustomModal(modifier: Modifier = Modifier,
+                navController: NavController,
+                openDialogCustom: MutableState<Boolean>,) {
     Card(shape = RoundedCornerShape(10.dp),
-        modifier = Modifier.padding(10.dp,5.dp,10.dp,10.dp).shadow(elevation = 1.dp),
+        modifier = Modifier
+            .padding(10.dp, 5.dp, 10.dp, 10.dp)
+            .shadow(elevation = 1.dp),
     elevation = 8.dp) {
         Column(modifier = Modifier
             .background(Color.White)
@@ -43,7 +49,10 @@ fun CustomModal(modifier: Modifier = Modifier, openDialogCustom: MutableState<Bo
             Row(modifier= Modifier
                 .padding(top = 10.dp)
                 .fillMaxWidth(),) {
-                OutlinedButton(onClick = { /*TODO*/ },
+                OutlinedButton(onClick = { /*TODO*/
+                                         openDialogCustom.value = false
+                    navController.navigate("afterEnd")
+                                         },
                     border = BorderStroke(0.5.dp, TextColor),
                     modifier = Modifier.fillMaxWidth(0.5f),
                     shape = RoundedCornerShape(bottomStart = 10.dp)) {
@@ -51,7 +60,8 @@ fun CustomModal(modifier: Modifier = Modifier, openDialogCustom: MutableState<Bo
                         color = TextColor,
                          fontSize = 18.sp,fontFamily = fontSansFamily,)
                 }
-                OutlinedButton(onClick = { /*TODO*/ },
+                OutlinedButton(onClick = { /*TODO*/
+                                         openDialogCustom.value = false},
                     border = BorderStroke(0.5.dp, TextColor),
                     modifier = Modifier.fillMaxWidth(1f),
                     shape = RoundedCornerShape(bottomEnd = 10.dp)
@@ -70,5 +80,7 @@ fun CustomModal(modifier: Modifier = Modifier, openDialogCustom: MutableState<Bo
 @Preview(name="Custom Dialog")
 @Composable
 fun MyDialogUIPreview(){
-    CustomModal(openDialogCustom = mutableStateOf(true))
+    val navController = rememberNavController()
+    CustomModal(openDialogCustom = mutableStateOf(true),
+        navController = navController)
 }
